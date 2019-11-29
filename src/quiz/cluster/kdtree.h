d98db5ecd9397d24d6aaf35212b226e5549600cc
Luis Felipe Wolf Batista <luisfelipewb@gmail.com>
2019-11-29 17:30:37 -0300

    Reduce variables

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -31,41 +31,35 @@ struct KdTree
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root
 		int tree_depth = 1;
-
-		Node* new_node = new Node(point, id);
 		Node* insertLocation = root;
 
-
 		if (root == NULL)
-		{
-			root = new_node;
-			std::cout << "Add root Node" << std::endl;
-		}
+			root = new Node(point, id);
 		else
 		{
 			while (insertLocation != NULL)
 			{
 				tree_depth++;
 
-				if (point[tree_depth % 2] < insertLocation->point[tree_depth % 2])
+				if (point[tree_depth%2] < insertLocation->point[tree_depth%2])
 				{
 					if (insertLocation->left == NULL)
 					{
-						insertLocation->left = new_node;
+						insertLocation->left = new Node(point, id);
 						break;
 					}
 					else
-					insertLocation = insertLocation->left;
+						insertLocation = insertLocation->left;
 				}
 				else
 				{
 					if (insertLocation->right == NULL)
 					{
-						insertLocation->right = new_node;
+						insertLocation->right = new Node(point, id);
 						break;
 					}
 					else
-					insertLocation = insertLocation->right;
+						insertLocation = insertLocation->right;
 				}
 			}
 		}
